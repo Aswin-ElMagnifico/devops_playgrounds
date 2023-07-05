10291d515b5736749dfa47c828527cdcea2842fa
Aswin <43544673+Aswin-ElMagnifico@users.noreply.github.com>
2023-07-05 16:22:55 +0530

    Update main; add env print

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
+import os
+
+
 def greeter_message():
     return "Hello world!"
 
 
+def print_envs():
+    print(os.environ.get("API_KEY"))
+
+
 if __name__ == '__main__':
     print(greeter_message())
+    print_envs()
