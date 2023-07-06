b098277229feb2bc9caf9c71dfccce7cf474f259
Aswin <43544673+Aswin-ElMagnifico@users.noreply.github.com>
2023-07-06 11:47:22 +0530

    Revert main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,6 @@
-import os
-
-
 def greeter_message():
     return "Hello world!"
 
 
-def print_envs():
-    if os.environ.get("API_KEY") == "abcdefg":
-        print("abcdefg")
-    else:
-        print("encrypted")
-
-
 if __name__ == '__main__':
     print(greeter_message())
-    print_envs()
